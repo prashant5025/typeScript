fa28b151b193b22766912e81f73b09730792f869
Prashant Singh <psingh5025615@gmail.com>
2023-04-03 02:27:48 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,60 +1,41 @@
 // let num1 = "a";
-
 // function calculate(num1: number, num2: number){
 //     return num1 + num2;
 // }
-
 // console.log(calculate(num1, 2));
-
 // function getTotal(numbers: number[]) {
 //     return numbers.reduce(function (total, num) {
 //         return total + num;
 //     },0);
 // }
-
 // const user = {
 //     name: "John",
 //     age: 30,
 //     role: 'professor'
-    
-    
 // }
-
 // console.log(user.name,user.age);
-
 // type alias
-
 // type User = {
 //     name: string;
 //     age: number;
 //     role?: string;
-
 // }
-
 // const user: User = {
 //     name: "prashant",
 //     age: 30
 // }
-
 // function login(userData: User): User {
 //     return userData;
 // }
-
 // console.log(login({name: "prashant", age: 30, role: "professor"}))
-
-
 //costume types
 // type ID = number | string;
-
 // const userId: ID = '123';
-
 // // interface
-
 // interface Transaction {
 //     payerAccountNumber: number,
 //     payeeAccountNumber: number,
 // }
-
 // interface BankAccount {
 //     accountNumber: number,
 //     accountHolderName: string,
@@ -62,7 +43,6 @@
 //     isActive: boolean,
 //     transaction: Transaction[]
 // }
-
 // const bankAccount: BankAccount = {
 //     accountNumber: 123456789,
 //     accountHolderName: "Prashant",
@@ -74,29 +54,19 @@
 //             payeeAccountNumber: 987654321
 //         }
 //     ]
-    
-
 // }
-
 // console.log(bankAccount);
-
 // interface Book {
 //     name: string;
 //     price: number;
 // }
-
 // interface Ebook extends Book {
 //     fileSize: number;
 //     format: string;
 // }
-
 // interface audioBook extends Ebook{
-
 //     duration: number;
 // }
-
-
-
 // const book: audioBook = {
 //     name: "The Alchemist",
 //     price: 100,
@@ -104,23 +74,18 @@
 //     format: "pdf",
 //     duration: 5
 // }
-
 // console.log(book);
-
 // type Book = {
 //     name: string;
 //     price: number;
 // }
-
 // type Ebook = Book & {
 //     fileSize: number;
 //     format: string;
 // }
-
 // type audioBook = Ebook & {
 //     duration: number;
 // }
-
 // const book: audioBook = {
 //     name: "The Alchemist",
 //     price: 100,
@@ -128,11 +93,8 @@
 //     format: "pdf",
 //     duration: 5
 // }
-
 // console.log(book);
-
 // type ID = number | string;
-
 // // Narrowing
 // function printID(id: ID) {
 //     if (typeof id === 'string'){
@@ -142,9 +104,7 @@
 //     }
 // }
 // printID("number");
-
-function getFirstThree(x: string | number[]) {
-    return x.slice(0,3);
+function getFirstThree(x) {
+    return x.slice(0, 3);
 }
-
-console.log(getFirstThree([1,2,3,4,5,6,7,8,9,10]));
+console.log(getFirstThree([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
